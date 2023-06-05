0499f0ada04307c5d111ab59439e18396aff4b03
Sergio Peralta <seperalta88@gmail.com>
2023-06-05 12:15:24 -0300

    Add hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,34 @@
-const Header = () => (
-  <div>Header</div>
-);
-export default Header;
+import { useState, useRef } from 'react';
+import useOnClickOutside from '../hooks/useOnClickOutside';
+
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const ref = useRef();
+
+  useOnClickOutside(ref, dropdown, () => setDropdown(false));
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li ref={ref}>
+          <button type="button" onClick={() => setDropdown(!dropdown)}>
+            Services
+            {' '}
+            <span>&#8595;</span>
+          </button>
+          {dropdown && (
+          <ul>
+            <li>Design</li>
+            <li>Development</li>
+          </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
