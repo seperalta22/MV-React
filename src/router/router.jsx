b845ad2b819e12a8384e29aaa08e8de5d0fe7692
Sergio Peralta <seperalta88@gmail.com>
2023-06-05 15:04:43 -0300

    Create about page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
+import App from '@/App';
+import About from '@/pages/About';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/about',
+		element: <About />,
+	},
 ]);
 
 export default router;
