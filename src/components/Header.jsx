01e115d3ced60d090501f3233a9d81a433407d59
Sergio Peralta <seperalta88@gmail.com>
2023-06-05 14:41:44 -0300

    Update header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
               <span>&#8595;</span>
             </button>
             {dropdown && (
-            <ul>
+            <ul className={styles.dd}>
               <li>Design</li>
               <li>Development</li>
             </ul>
